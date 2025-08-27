d5df40e264ba3dce08b103b33976fd019ba2e519
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-08-27 08:09:45 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,128 @@
+# print("hello world!")
+# print("hello boss")
+# print("hello world!","hello boss this is for concatenation")
+# print(23)
+# print(35)
+# # print(23+24)
+# # print(20-20)
+# # print("print:it display  anything on the screen")
+# # print("variable means memory location")
+# # name ="shradha"
+# # print("double quarts we declare this as a string")
+# # age = 34
+# # price = 234454
+# # print("name")
+# # print(age)
+# # print(price)
+# # # print("my age is:",age)# print(type(age))
+# # # print(type(price))
+# # # print(type(name))
+# # # print("int is like postive, negative,zero etc")
+# # # a = 2
+# # # b =3
+# # # sum = a+b
+# # # print("sum")
+# # # #print("comments")
+# # # ''' hay how'''
+
+# # #operator: operators it s work is to perform operation 
+# # #arithmatic operator
+
+# # a = 4
+# # c = 3
+# # sum = a + c
+# # print (sum)
+# # print(a+c)
+# # print(a-c)
+# # print(a/c)
+# # print("operators")
+
+
+# # s = 4
+# # m = 3
+
+# # print(s == m)
+# # print(s != m)
+
+# # print(s <= m)
+# # print(s > m)
+# # print(s > m)
+# # #assignment operators
+
+
+# # num = 40
+# # num = num  + 34
+# # num +=24
+
+# # print( "the total value is:",num)
+# #there r three logical operators in python
+# #not and or
+
+# a = 4
+# b = 44
+# print(not False)
+# print (not True)
+
+# print(not(a>b))
+
+
+# # v1 = True
+# # v2 = False
+# # print("and operator", v1  or v2)
+# # print("type conversion")
+
+
+# # a = 4
+# # b = 3.4
+# # sum = a + b
+# # print (sum)
+
+# # #type casting is used to convert one data type to second data type
+
+
+a = 3.43
+b = str(a)
+print(b)
+print (type(b))
+
+# print("hello")
+
+
+
+# name =input("enter you id:")
+
+# print(name)
+
+# first = input("enter you first name:")
+# second =input ("enter you sencond name:")
+
+# print("enter please:",first)
+# print("enter please:",second)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
