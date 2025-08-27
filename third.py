29ddb4ef61c9586a311a9edbf5aefb0399e704a7
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-08-27 08:10:14 +0530

    Add files via upload

---
+++ third.py
@@ -0,0 +1,10 @@
+# WAP to input 2 floating point numbers and print their average
+
+num1 = float(input("enter the first number"))
+num2 = float(input("enter the second number"))
+# typecast means convert data in to anothe data type
+average =( num1+num2) /2
+
+print(average)
+# average is equal to (a+b)/2
+
