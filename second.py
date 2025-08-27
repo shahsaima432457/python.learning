26616b800134bfd5f0a780cec3ad09a656f5e784
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-08-27 08:09:19 +0530

    Add files via upload

---
+++ second.py
@@ -0,0 +1,5 @@
+# write a program  to input side of a square and print its area
+side = float(input("enter the length of the square"))
+
+print("area: ",side*side)
+#area is equal to  length *lenght
