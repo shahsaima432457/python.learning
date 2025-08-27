796914ae0c17b0c79fa09c2b7d84a20fcb84c87c
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-08-27 08:06:36 +0530

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,12 @@
+# write a program to input 2 numbers and print their sum
+a = 67
+b = 23
+sum = a + b
+print(sum)
+#the above program  is in the simple method
+#the below program i can create through the input method
+
+first = int(input("enter the first number"))
+second =int(input("enter the second number"))
+sum = first +second
+print(sum)
