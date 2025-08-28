e518c0bf3ac3b5f52fc1e7a2a6cab6dedfda86bd
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-08-28 09:03:26 +0530

    Add files via upload

---
+++ grade studentsmarks.py
@@ -0,0 +1,12 @@
+
+marks = 35
+if(marks >= 90):
+    print("grade A")
+elif(marks >90  and marks>= 80):
+    print("grade B")
+    
+elif( marks >80 and marks >= 70):
+    print ("grade c")
+else:
+    print("grade D")
+    
