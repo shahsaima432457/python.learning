9d46ee40a2a1ac63d432e0ccf29d4e7d23e3f01a
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-08-28 09:04:37 +0530

    Add files via upload

---
+++ nesting.py
@@ -0,0 +1,13 @@
+# nesting means one statement have sub statements
+# if statement have inside another if statement
+
+age = 11
+# nesting
+if(age >= 15):
+    if(age >=80):
+        print("drive")
+    else:
+        print("not drive")
+        
+else:
+    print("cannot drive")
