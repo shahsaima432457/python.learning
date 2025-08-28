ad7e7ff12ecf1a3fbac7d29256ef8298ddaa9cdd
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-08-28 09:04:07 +0530

    Add files via upload

---
+++ if-elif-else.py
@@ -0,0 +1,56 @@
+
+'''
+if(condition):
+statement
+elif(condion)
+statement
+else
+statement
+'''
+# age = 24
+# if(age == 24):
+#         print("true")
+# elif(age >= 20):
+#     print("True")
+# else:
+#     print("False")
+
+
+light ="green"
+
+if(light == "green"):
+    print("go")
+    
+elif(light == "red"):
+
+    print("stop")
+    
+elif(light == "yellow"):
+    print("w8")
+    
+    # if statement always check the condition
+    # and elif statement  checks when startment is wrong the it will switch in to this
+
+#write a program  if the age is 70 retire otherwise not
+
+ht = 85
+if(ht >= 80):
+    print("the person will get retire")
+else:
+    print("the person will not get retire")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
