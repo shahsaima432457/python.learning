8d00328847a0211a073d32cc96b2b2264c5abd8a
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-08-29 08:57:33 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,45 @@
+# list is a built in data type that stores set of values
+# it can store elements of different types (integer, float, string, etc.)
+
+# marks = [40.4, 45, 34.4, 12]
+# print(marks)
+# print(type(marks))
+
+# list methods
+
+list  = [2,1,3]
+print(list)
+#append means kise cheez ko last may jood dana
+# means join any thing at the end only one element it can add
+
+list.append(0)
+list.sort()
+# sort is used to arrange the elements by sort method
+
+list.sort(reverse=True)
+
+print(list)
+
+# insert method is simler to append method
+# insert element at index
+# insert(index,element)
+list.insert(1,5)
+print(list)
+#with the help of insert method we can insert the values in anyware of the elements list
+
+
+
+
+
+
+
+
+#remove methods
+list1 =[0,1,2,3,4,5,6]
+list1.remove(1,)
+#remove  first occurance of element
+print(list1)
+
+
+list1.pop(4)
+print(list1)
