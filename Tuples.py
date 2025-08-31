9181b33388b6dc9696c6bbafb4d9f402cf369a39
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-08-31 22:32:53 +0530

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,37 @@
+# # # Tuple is a data type .
+# # #Tuple is a built in data type that lets us create immutable sequences of values.
+
+# # # list is multiple data type  which has change Values
+# # # Tuple is a immutable data type which can not change values
+
+# # tp = (1,2,3)
+# # print(tp)
+# # print(type(tp))
+
+
+# # Write to ask the user to enter names of their 3 favourite movies and store them in a list
+# movies =[]
+# first =input("enter the first name of the movie: ")
+# second = input("enter the second name of the movie:")
+# third = input ("enter the third name of the movie")
+
+# movies.append(first)
+# movies.append(second)
+# movies.append(third)
+
+# print(movies)
+
+# wap to check if a list contains a palindrome of elements.
+#palindrome the things which is same as front and back
+# example 12321
+# list [1, 2 ,3 ,2, 1]
+
+# copy_list1 = list.copy()
+
+# copy_list1.reverse()
+
+# if(copy_list1 ==list):
+    
+#     print("palindrome")
+# else:
+#     print("not")
