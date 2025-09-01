0ecab2f38f497aa91fbd40ad820b573c96713882
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-09-01 23:56:46 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,48 @@
+# it is a important data structure
+#it is a build in data type
+# dictionaries are used to store data  values in key-value pairs
+#they are unordered, mutable(changeable) and dont allow duplicate keys
+
+# key - value
+#word- meaning
+
+dictionary1 = {
+    "name " :"sam",
+    "no" :"1",
+    "language" : "urdu",
+    "subjects" : ["english", "urdu","arabic"],
+    "topics ": ("dict", "set")
+}
+
+print(dictionary1)
+
+print(dictionary1["no"])
+
+print(dictionary1 ["language"] )
+
+print(dictionary1 ["name "])
+
+
+#empty dictionary
+
+# i  want to create a  null dictionary
+
+null_dictionary = {
+    
+
+
+}
+print(null_dictionary)
+
+
+# i want to add the values of the above dictionary
+
+dictionary = {} 
+    
+
+dictionary["name"] = "hk"
+
+
+print(dictionary)
+
+
