13c5b8de5c06e84850eedc42ba27a3a79ecdf4bc
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-09-01 23:55:48 +0530

    Add files via upload

---
+++ class and object.py
@@ -0,0 +1,39 @@
+# object oriented programming:
+# object
+# class:it is a blue print of an object
+# object : in real world object is anything like mouse, keyboard etc
+
+# class describes the object
+# object  real world entity
+
+# creating a simple class
+# class name   first alphabetic in caps
+
+
+# class House:
+#     # class name is house
+# # create an variable in the class
+#     rooms ="4"
+#     washrooms="2"
+#     lobby="1"
+    
+# # create an object for print
+# demo =House()
+# print(demo.rooms)
+# print(demo.washrooms)
+# print(demo.lobby)
+
+# using function inside the class
+
+class Design:
+    # using function
+    a=10
+    
+
+    def value(self):
+        print(20+30)
+        
+# create an object for this
+demo= Design()
+print(demo.a)
+demo.value()
