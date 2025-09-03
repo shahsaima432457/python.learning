56158655d665f188d345432f4eec84d1310b5812
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-09-03 08:33:16 +0530

    Add files via upload

--- practice.py
+++ practice.py
@@ -1,18 +1,79 @@
+# # practice no 1
+# dictionary ={
+    
+# "Cat" : "a small animal",
+# "table" : ["a piece of furniture","list of facts and figures"]
 
-# WAP to count the number of students with the "A " grade in to the tuple
 
-# tuple1 = ("c","d","a","a","b","b","a")
-
-# print(tuple1.count("a"))
+# }
+# # print(dictionary)
 
 
-#store the above values in a list and sort them from  A to D
+# # # practice no 2
+# # # you are given a list of subjects for students. ASsume one classroom is required for 1 subject. How man classrooms are needed by all students
 
-list = ["c","d","a","a","b","b","a"]
+# # classroom = {"python","java","c++","python","javascript","c"}
 
-# print(list.sort())
-# list.sort()
+# # print(classroom)
+# # print(len(classroom))
 
-# print(list)
+# # #practice no 3
+# # # WAp to enter marks of 3 subjects from the user and store them in a dictionary.
+# # #Start with an empty dictionary and add one by one . use subject name as key and marks as value
+
+# dictionarya = {}
+
+
+# user =int(input("enter the marks of the phy subject: "))
+
+# dictionarya.update({"phy":user})
+
+
+# user = int(input("enter the marks of the chy subject: "))
+
+# dictionarya.update({"chy":user})
+
+# user = int(input ("enter the marks of the eng subject: "))
+
+# dictionarya.update({"eng":user})
+
+# print(dictionarya)
+
+
+# figure out a way to store  9 and 9.0 as separate  values in the set.
+#also can create with the help of build in data types
+
+
+
+values ={ 9, 9.0}
+
+print(values)
+
+#it shows the out only nine because the two values are same  as i already know that set can not print dublicate values
+
+
+
+values ={ 9, 9.45}
+
+print(values)
+# with the help of data types
+
+value = {
+    ("float",9.0)
+
+}
+
+print(value)
+
+
+
+
+
+
+
+
+
+
+
 
 
