2e3443a93eb65449dc15c2201b6bc9ef8b292eb3
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-09-04 23:46:51 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,88 @@
+# loops are used to repeat instructions.
+#while loops
+#qno1
+# i = 0
+
+# while i <= 5:
+# # iterations
+#     print("hi")
+
+# i +=1
+# #Qno2
+
+# # wap to print  college in five times
+
+# j = 0
+
+# while(j <=5):
+    
+#     print(" college")
+
+# j += 1
+
+
+#qno3 print the numbers from 1 to 5
+
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+# print("end loop")
+
+#wap to print  numbers from five to one reverse order
+
+
+
+# i = 5
+# while i >= 1:
+#     print(i)
+#     i -= 1
+# print("end loop")
+
+# print numbers from 1 to 100
+# using while loop
+
+i = 1
+
+while i <= 100:
+    print(i)
+    i += 1
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
