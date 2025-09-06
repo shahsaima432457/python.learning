e597f38a4f5b9d7d1e568daa774142a84660f64d
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-09-07 00:47:10 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,85 @@
+
+# for loops are used  for sequential traversal.
+# traversing a list, string, tuple
+#syntex
+#for element in list:
+#print(element)
+# for loop using list
+a =[1,2,3,4,5,6,]
+
+for num in a:
+    print(num)
+    
+
+
+# for loop using string list
+
+b =["hi", "hello", "hy"]
+
+for m in b:
+    print(m)
+    
+
+
+
+
+
+# for loop using tuple in list
+
+
+# abc =(1,2,3,4,5,6,7,8,9,)
+
+# for good in abc:
+#     print(good)
+    
+
+# for loop with else
+# for element in list:
+#some work
+#else
+#work with loop ends
+
+abc =(1,2,3,4,5,6,7,8,9,)
+
+for good in abc:
+    print(good)
+    
+
+# the work which we want to till end that is why we use else statement
+
+
+# abc =" goodmorning"
+
+# for d in abc:
+#     if(d == ):
+#         print("ooo")
+#     break
+#     print(d)
+#     print("g")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
