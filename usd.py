f4c4934d43f344e344655684e598f06010427556
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-09-18 08:07:09 +0530

    Add files via upload

---
+++ usd.py
@@ -0,0 +1,26 @@
+# wap  to convert USD to INR
+# 1 dollar to  83 rupees
+
+def convert(usd_value):
+    india_value =usd_value *83
+    print(usd_value*83)
+    print(usd_value,"USD =",india_value,"INR")
+    return india_value
+
+
+convert(2)
+convert(3)
+convert(4)
+
+
+
+# convert the dirham to in  india rupees
+
+def converter(dirham):
+    indian_value = dirham * 23
+    print(indian_value)
+    print(indian_value,"IND=",dirham, "DIR=")
+    return indian_value
+
+
+converter(5000)
