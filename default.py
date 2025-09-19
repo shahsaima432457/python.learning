213eaacddf35917845b8b60cd71ed5174702ca52
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-09-19 23:46:17 +0530

    Add files via upload

---
+++ default.py
@@ -0,0 +1,32 @@
+# default parameters assigning a default value to parameter, which is used when no argument is passed
+# wrtie a program to calculate the multiplication of two numbers
+
+
+# def mul(a,b):
+#     print(a*b)
+#     return a * b
+    
+
+
+# mul(3,4)
+
+
+
+# default values
+
+
+def mul(a = 2,b = 2):
+    print(a*b)
+    return a * b
+    
+
+
+mul(4)
+
+
+
+
+
+
+
+
