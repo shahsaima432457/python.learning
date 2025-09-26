1ba317ba1e75092ab117870255a373a334105d96
shahsaima432457 <160920233+shahsaima432457@users.noreply.github.com>
2025-09-26 09:20:55 +0530

    Add files via upload

---
+++ practiceone.py
@@ -0,0 +1,32 @@
+
+# build in function: means print, range, type, it is the functions which is already in python, we dont need to write it 
+# user defined functions are those functions which  we write in the python
+
+
+#wap to print the length of a list (list is the parameter)
+# in simple way
+cities =["delhi","mumbai","kashmir","ladakh","shimla","ffff"]
+print(len(cities))
+
+
+# in function way
+
+
+
+# def function(cities):
+    
+
+#     cities =["delhi","mumbai","kashmir","ladakh","shimla"]
+#     print(len(cities))
+#     return cities
+    
+
+# two list at that time we call the parameter list
+
+
+
+
+places = ["urdu","english", "hindi","kashmiri"]
+def first(places):
+    
+    print(len(places))
